5f9041e7763596dd6fb1a3cf466b7ca8c32bd161
Bishal Saha <bishalsaha2623@gmail.com>
2025-12-29 00:48:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+function display(){
+  document.getElementById("govofandra").style.opacity = 1;
+    document.getElementById("mhrd").style.opacity = 1;
+}
+
+var slideIndex = 0;
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function visitcount(){
+  ++x;
+  document.getElementById("visit").innerHTML = temp;
+  return x;
+}
+
+
+
+
+
+
+
+
+
+// Student Login
+
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+
+function register(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login(){
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
