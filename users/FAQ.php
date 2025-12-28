ff8e96e9175381da4b57837aacb63f0a2efffe8d
Bishal Saha <bishalsaha2623@gmail.com>
2025-12-29 00:50:44 +0530

    Add files via upload

---
+++ users/FAQ.php
@@ -0,0 +1,74 @@
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Frequently ask questions</title>
+  <link rel='stylesheet' href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'>
+<link rel="stylesheet" href="FAQ.css">
+</head>
+<body>
+  <div class="container">
+    <h1 style="float: left;">Frequently Asked Questions</h1><br><br><br>
+
+    <i><h2>Recent Activity Questions</h2></i>
+
+    <div class="accordion">
+      <div class="accordion-item">
+        <a>What is a students total Expenses of Cynoaure 2020?</a>
+        <div class="content">
+          <p>A grievance is a formal method for a student to complain about a teacher for conduct perceived as unfair. Most of the time, grievances tend to be about grades that a student believes are undeserved. <br>
+            <a href ="Budget.pdf">Click here for the pdf file of expeneses.</a> </p>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <a>SHOULD A DEPARTMENT BE RECEIPTING IN EFTS (ACH FOR A GRANT) OR SHOULD IT BE DONE BY THE AUDITOR?</a>
+        <div class="content">
+          <p>If the ACH is being deposited into the County bank account, only the department should not include that amount on their report of collections.  They should provide the information to the Auditor and Treasurer sufficient to be able to post the ACH payment properly.</p>
+        </div>
+      </div>
+    </div>
+
+  </div><br>
+<div class="container">
+
+  <i><h2>Featured Questions</h2></i>
+
+  <div class="accordion">
+    <div class="accordion-item">
+      <a>What is a student grievance?</a>
+      <div class="content">
+        <p>A grievance is a formal method for a student to complain about a teacher for conduct perceived as unfair. Most of the time, grievances tend to be about grades that a student believes are undeserved.</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <a>SHOULD A DEPARTMENT BE RECEIPTING IN EFTS (ACH FOR A GRANT) OR SHOULD IT BE DONE BY THE AUDITOR?</a>
+      <div class="content">
+        <p>If the ACH is being deposited into the County bank account, only the department should not include that amount on their report of collections.  They should provide the information to the Auditor and Treasurer sufficient to be able to post the ACH payment properly.</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <a>HOW DO WE GET DEPARTMENTS NOT TO LEAVE THEIR MONEY IN THE AUDITOR'S OFFICE BEFORE RECEIPTING INTO TREASURY?</a>
+      <div class="content">
+        <p>Departments need to follow procedures for turning in their departmental collections and report of collections.   The custody of those collections does not pass to the Treasurer until the Treasurer has issued a Treasurer's receipt and taken custody of the collections for deposit.  If money is left with the Auditor, any loss of that money will be the responsibility of the department.</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <a>Is FAQ Section Matters In Website?</a>
+      <div class="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <a>FOR STATE EMPLOYEES ONLY: HOW DO I RESET MY PASSWORD?</a>
+      <div class="content">
+        <p>State employees can utilize the Self-Service Password Management (SSPM) system to reset passwords or unlock accounts. The password reset system is a service provided by Indiana Office of Technology's Customer Service (ICS) that allows users to reset their passwords without speaking to an ICS representative. During enrollment, users answer a series of security questions that are later used to verify their identity during the password reset process.</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<script src='https://code.jquery.com/jquery-3.2.1.min.js'></script>
+<script  src="function.js"></script>
+
+</body>
+</html>
